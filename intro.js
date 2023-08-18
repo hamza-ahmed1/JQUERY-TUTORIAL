489748d4a9a3684986c5d414c59cf058a25dba91
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-08-18 01:33:03 -0700

    Lec 03 done
    how to connect
    1-by creating a file .js and copy all jqery code and src before ending body tag
    2-by cdn
    */
    
    /*
       Basic Selectors
       1-select by id
       2-select by class
       3-select by tag name
    
       basic syntax: $(selector).Action()

---
+++ intro.js
@@ -0,0 +1,22 @@
+/*
+Jquery is a javascript library
+easy to use with ajax and html dom
+provide short methods for long javascript code
+*/
+
+/*
+how to connect 
+1-by creating a file .js and copy all jqery code and src before ending body tag
+2-by cdn 
+*/
+
+
+/*
+   Basic Selectors
+   1-select by id
+   2-select by class
+   3-select by tag name
+
+   basic syntax: $(selector).Action()
+*/
+
